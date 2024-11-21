f13ebc39194b5809554ca1388c1a2e3e9bfd0522
noRan56 <noran.nassef12@gmail.com>
2024-11-21 13:46:46 +0200

    - prepare project

---
+++ lib/bookly.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
